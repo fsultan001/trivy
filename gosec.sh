2c8cbcad69e3e0f860a603b506ebe55e4fac5720
fsultan001 <87090953+fsultan001@users.noreply.github.com>
2022-01-06 17:06:44 -0500

    Update gosec.sh

--- gosec.sh
+++ gosec.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-curl -sfL https://raw.githubusercontent.com/securego/gosec/master/install.sh | sh -s -- -b $(go env GOPATH)/bin vX.Y.Z
-
-wget https://github.com/securego/gosec/releases/download/vX.Y.Z/gosec_vX.Y.Z_OS.tar.gz
 
 gosec --help
