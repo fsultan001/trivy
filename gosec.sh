d19789cda03192f7dff123f2745a630ff40f59e7
fsultan001 <87090953+fsultan001@users.noreply.github.com>
2022-01-07 12:30:45 -0500

    Update gosec.sh

--- gosec.sh
+++ gosec.sh
@@ -2,5 +2,5 @@
 
 curl -sfL https://raw.githubusercontent.com/securego/gosec/master/install.sh | sh -s latest
 
-gosec -fmt=json -out=gosec-results.json ./...
+./bin/gosec -fmt=json -out=gosec-results.json ./...
 
