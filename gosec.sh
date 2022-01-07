156e76b138a9ee65b237930d0790375340c3515a
fsultan001 <87090953+fsultan001@users.noreply.github.com>
2022-01-07 13:26:41 -0500

    Update gosec.sh

--- gosec.sh
+++ gosec.sh
@@ -4,9 +4,9 @@ curl -O https://storage.googleapis.com/golang/go1.13.5.linux-amd64.tar.gz
 
 tar -xvf go1.13.5.linux-amd64.tar.gz
 
-mv go /usr/local
+mv go /var/jenkins_home/usr/local
 
-export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
+export PATH=$PATH:/var/jenkins_home/usr/local/go/bin:$GOPATH/bin
 
 curl -sfL https://raw.githubusercontent.com/securego/gosec/master/install.sh | sh -s latest
 
