8827bff774b16d13ce8b7871686a14fcf25f91ab
fsultan001 <87090953+fsultan001@users.noreply.github.com>
2022-01-07 12:12:58 -0500

    Update gosec.sh

--- gosec.sh
+++ gosec.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-export PATH=$PATH:/usr/local/go/bin:$GOPATH/bin
+apt install golang-go
+
+go install github.com/securego/gosec/v2/cmd/gosec@latest
 
 gosec -fmt=json -out=gosec-results.json ./...
 
